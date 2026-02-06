6de52fd536fd20c9c3167bcc08d2c33a20c4c777
Kerem Gavcar <kerem1712000@hotmail.com>
2026-02-06 09:48:43 +0300

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,23 +6,40 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClampRange =5f;
+    [SerializeField] float yClampRange = 5f;
+
+
+    [SerializeField] float controlRollFactor = 20f;
 
     Vector2 movement;
+
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
     }
+    
 
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
     }
-    private void ProcessTranslation()
+    void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
-        float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
-    }
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
 
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange );
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+    void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f,0f,-controlRollFactor * movement.x);
+        transform.localRotation = targetRotation;
+    }
 }
